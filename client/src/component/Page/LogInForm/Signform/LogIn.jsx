20ea4a75787eaccb0c3d023a03f6c9921684ac0d
bruxellesflorian <florianbertchi@gmail.com>
2021-04-01 14:31:31 +0200

    débug OK ! Co-authored-by: Jean Fabry <JeanFabry@users.noreply.github.com> Co-authored-by: Joëlle  <Joelle-Everaert@users.noreply.github.com>

--- client/src/component/Page/LogInForm/Signform/LogIn.jsx
+++ client/src/component/Page/LogInForm/Signform/LogIn.jsx
@@ -28,7 +28,7 @@ const LogIn = () => {
       if (res.data.admin){
         window.location = '/AdminPage'
       }else{
-        window.location = '/New' // A MODIFIER SI ON VEUT 
+        window.location = '/Adre' // A MODIFIER SI ON VEUT 
         console.log(res.data);
       }
     })
@@ -37,7 +37,6 @@ const LogIn = () => {
     })
 
   }
-  https://prod.liveshare.vsengsaas.visualstudio.com/join?04CE76529922F5B71D65B72613B3052403F8
     return (
       <div id="login">
         <h1>Login</h1>
