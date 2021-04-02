246579971ba8d18f83f3104333da02100c37a42f
JeanFabry <jeanfabry1435@gmail.com>
2021-04-02 12:44:34 +0200

    names update when logged in

--- client/src/component/Page/admin/AdminHeader.jsx
+++ client/src/component/Page/admin/AdminHeader.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useState, useContext } from 'react'
 import { Link } from 'react-router-dom'
+import { UidContext } from '../../AppContext';
+import axios from 'axios';
+
+
 
 const AdminHeader = () => {
+    const { uid, loading } = useContext(UidContext)
+    const [admin, setAdmin] = useState('');
+ 
+    const getName = async () => {
+        try {
+        const res = await axios({
+            method: "get",
+            url: `${process.env.REACT_APP_API_URL}api/user/`+ uid.id,
+            withCredentials: true
+        })
+        setAdmin(res.data.first_name);
+        }
+        catch(err){
+            console.log(err);
+            }
+    }
+    getName();
+    
+
+
     return (
         <>
             <div className="userHeader">
-                <h1>Hello Admin!</h1>
+                <h1>Hello { admin }!</h1>
                    <div className="left">
                     <Link to="/AdminDashboard">Dashboard</Link>
                    <Link to="/AddReference">Add a reference</Link>
